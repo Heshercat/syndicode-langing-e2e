a6087d7e8540aaa4ec954e6b39b765f932f1c5ad
MosiichukO <mosiychuk1998a@gmil.com>
2022-12-17 00:21:41 +0200

    Added test example

--- services/pages/home.page.ts
+++ services/pages/home.page.ts
@@ -1,17 +1,9 @@
 import type { Locator, Page } from  '@playwright/test';
-import { expect } from '@playwright/test';
-export class HomePage{
+
+export default class HomePage{
     readonly page: Page
     constructor(page:Page){
         this.page=page
     }
-    async typeSearchText(){
-       await this.page.type('input[name="q"]',"Playwright")
-    }
-    async pressEnter(){
-       await this.page.keyboard.press('Enter');
-    }
-    async searchResult(){
-       return this.page.innerText('//h3[contains(text(),"Playwright:")]')
-    }
+
 }
