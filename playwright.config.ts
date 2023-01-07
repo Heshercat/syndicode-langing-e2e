2994d0b4694df6b2454c0d70cc8ceb19396ff10e
MosiichukO <78620006+MosiichukO@users.noreply.github.com>
2023-01-07 19:29:56 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ const config: PlaywrightTestConfig = {
   use: {
     baseURL: envURLs.prod,
     actionTimeout: 0,
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: 'on-first-retry',
